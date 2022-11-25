94da0935c9297eb628d8675e8c3c26cfa7db0cfc
Robert Marshall <hello@robertmarshall.xyz>
2022-11-25 00:18:14 +0000

    CNAME

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221125.081616';
+const cacheName = 'chirpy-20221125.081805';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
