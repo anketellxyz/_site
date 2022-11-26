e131142b5ade9a01c80910d60d2c4192179f3a4b
Robert Marshall <hello@robertmarshall.xyz>
2022-11-26 23:24:52 +0000

    changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221126.183749';
+const cacheName = 'chirpy-20221126.232434';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
